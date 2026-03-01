9a222f8e51f62f814c9facebe0b060aa8505c27d
MatthewJStevenson <matthewstevenson@outlook.com.au>
2026-03-01 19:34:22 +1100

    added fruit

---
+++ world.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_fruit_fruit_collected() -> void:
+	$Fruit/EatFruit.play()
+	$Fruit.position.x += 32
