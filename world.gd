d3fd7c4102a71b0f6e4d5d770ff2294525205979
Taylor <68413453+nealtaylor98@users.noreply.github.com>
2026-04-19 18:50:21 +1000

    Make fruit move to a random position when collected

--- world.gd
+++ world.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const x_offset = 58
+const y_offset = 144
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +16,7 @@ func _process(delta: float) -> void:
 
 func _on_fruit_fruit_collected() -> void:
 	$Fruit/EatFruit.play()
-	$Fruit.position.x += 32
+	var x = randi_range(0, 12)
+	var y = randi_range(0, 12)
+	$Fruit.position.x = x_offset + (x * 32)
+	$Fruit.position.y = y_offset + (y * 32)
