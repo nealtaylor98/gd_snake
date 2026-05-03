95a84967b934ce632c1183313fa12c44b2e40ac6
Taylor <68413453+nealtaylor98@users.noreply.github.com>
2026-05-03 16:22:32 +1000

    Add score tracking to game

--- world.gd
+++ world.gd
@@ -3,6 +3,8 @@ extends Node
 const x_offset = 58
 const y_offset = 144
 
+var score = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +18,12 @@ func _process(delta: float) -> void:
 
 func _on_fruit_fruit_collected() -> void:
 	$Fruit/EatFruit.play()
+	update_score()
 	var x = randi_range(0, 12)
 	var y = randi_range(0, 12)
 	$Fruit.position.x = x_offset + (x * 32)
 	$Fruit.position.y = y_offset + (y * 32)
+
+func update_score() -> void:
+	score += 1
+	$ScoreLabel/Score.text = str(score)
